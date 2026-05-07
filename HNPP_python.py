39afbc13a03156fa68f354867068fdbf3fc99d9b
zhongjiayuan-fs <47944158+zhongjiayuan-fs@users.noreply.github.com>
2026-05-07 23:24:02 +0800

    Update HNPP_python.py

--- HNPP_python.py
+++ HNPP_python.py
@@ -1,118 +1,156 @@
+import os
 import gudhi
 import numpy as np
-import numpy as np
-from scipy.stats import pearsonr
-import os
 import pandas as pd
 import matplotlib.pyplot as plt
-import networkx as nx
-
+from scipy.stats import t
 
 df = pd.read_csv("Pericyte_to_neuron.csv",index_col=0,header=None) 
-data_value = df.values
-data_value=np.log(data_value+1)
 
-TF_df = pd.read_csv("TF_human.csv",index_col=0,header=None)
-TF_df_list =TF_df.index
+data_value = df.values.astype(float)
+data_value = np.log1p(data_value)
 
-data_gene_list=df.index
+TF_df = pd.read_csv("TF_human_unique.csv", index_col=0, header=None)
+
+
+data_gene_list = df.index
 TF_result = pd.Series(0, index=data_gene_list)
 TF_result.loc[df.index.isin(TF_df.index)] = 1
-TF_result_list = TF_result.tolist()
-TF_result_array = np.array(TF_result_list)
+TF_result_array = TF_result.to_numpy(dtype=float)
+
 sum_values = np.sum(TF_result_array)
 if sum_values == 0:
-    normalized_array = np.zeros_like(TF_result_array)/len(TF_result_array)  # 赋予均匀分布
+    normalized_array = np.zeros_like(TF_result_array) / len(TF_result_array)
 else:
     normalized_array = TF_result_array / sum_values
 
-cell_num =[76,86,48,283,61,69]
+print(normalized_array)
+print("Normalized Array:", normalized_array)
 
-cell_count=0;
-PR_mean_value = []
-for k in range(len(cell_num)):
-    after_cell_count = sum(cell_num[0:k+1])
-    data_matrix = data_value[:, cell_count:after_cell_count]
-    cell_count = sum(cell_num[0:k+1])
-    data_matrix_size = data_matrix.shape[0]
-    p_values = np.ones((data_matrix_size, data_matrix_size))
-    corr_matrix = np.zeros((data_matrix_size, data_matrix_size))
-    for i in range(data_matrix_size):
-        for j in range(data_matrix_size):
-            corr, p_value = pearsonr(data_matrix[i, :], data_matrix[j, :])
-            p_values[i, j] = p_value
-            corr_matrix[i, j] = corr
+cell_num = [76, 86, 48, 283, 61, 69]  
+
+
+max_edge_length_threshold =0.2 
+
+damping_factor = 0.85
+factor_1 = 0.1
+factor_2 = 1 - damping_factor - factor_1
+epsilon = 1e-6
+
+
+def fast_corr_pvalue_matrix(data_matrix):
+
+    n_genes, n_cells = data_matrix.shape
+
+
+    corr_matrix = np.corrcoef(data_matrix)
+
+    with np.errstate(divide='ignore', invalid='ignore'):
+        t_stat = corr_matrix * np.sqrt((n_cells - 2) / (1.0 - corr_matrix ** 2))
+        p_values = 2 * t.sf(np.abs(t_stat), df=n_cells - 2)
+
+ 
     corr_matrix = np.nan_to_num(corr_matrix)
-    p_values = np.nan_to_num(p_values, nan=1)
+    p_values = np.nan_to_num(p_values, nan=1.0)
+
+    return corr_matrix, p_values
+
+
+PR_mean_value = []
+
+cum_cell_num = np.cumsum(cell_num)
+start_idx = 0
+
+for k, end_idx in enumerate(cum_cell_num):
+
+    data_matrix = data_value[:, start_idx:end_idx]
+    start_idx = end_idx
+
+    num_nodes = data_matrix.shape[0]
+
+
+    corr_matrix, p_values = fast_corr_pvalue_matrix(data_matrix)
+
     corr_matrix[p_values > 0.05] = 0
-    correlation_matrix = abs(corr_matrix)
+
+    correlation_matrix = np.abs(corr_matrix)
     distance_matrix = 1 - correlation_matrix
     
-    max_edge_length_threshold =0.2 
-    rips_complex = gudhi.RipsComplex(distance_matrix=distance_matrix, max_edge_length=max_edge_length_threshold)
+    rips_complex = gudhi.RipsComplex(
+        distance_matrix=distance_matrix,
+        max_edge_length=max_edge_length_threshold
+    )
     simplex_tree = rips_complex.create_simplex_tree(max_dimension=2)
-    edge_2_simplices_counts = {}
-    for simplex in simplex_tree.get_skeleton(1):
-        if len(simplex[0]) == 2:         
-            edge = tuple(sorted(simplex[0]))
-            edge_2_simplices_counts[edge] = 0
-            
-    for simplex in simplex_tree.get_skeleton(2):
-        if len(simplex[0]) == 3:   
-            vertices = simplex[0]
-            for i in range(3):
-                for j in range(i+1, 3):
-                    edge = tuple(sorted([vertices[i], vertices[j]]))
-                    if edge in edge_2_simplices_counts:
-                        edge_2_simplices_counts[edge] += 1
-                        
-    num_nodes = correlation_matrix.shape[0]
-    node_2_simplices_matrix = np.zeros((num_nodes, num_nodes), dtype=int)
-    for edge, count in edge_2_simplices_counts.items():
-        node1, node2 = edge
-        node_2_simplices_matrix[node1, node2] = count
-        node_2_simplices_matrix[node2, node1] = count      
-    
+
+
+    node_2_simplices_matrix = np.zeros((num_nodes, num_nodes), dtype=np.int32)
+
+    for simplex, _ in simplex_tree.get_skeleton(2):
+        if len(simplex) == 3:
+            a, b, c = simplex
+            node_2_simplices_matrix[a, b] += 1
+            node_2_simplices_matrix[b, a] += 1
+
+            node_2_simplices_matrix[a, c] += 1
+            node_2_simplices_matrix[c, a] += 1
+
+            node_2_simplices_matrix[b, c] += 1
+            node_2_simplices_matrix[c, b] += 1
+
+
     total_outdegree = np.sum(node_2_simplices_matrix, axis=1)
-    transition_matrix = node_2_simplices_matrix / (total_outdegree[:, None] + 0.0000001)
-    
-    
-    damping_factor = 0.85
-    factor_1=0.1
-    factor_2=1-damping_factor-factor_1
-    N = node_2_simplices_matrix.shape[0]
-    initial_PR = np.ones(N) / N
-    PR = initial_PR
-    epsilon = 1e-6
+    transition_matrix = node_2_simplices_matrix / (total_outdegree[:, None] + 1e-7)
+
+
+    N = num_nodes
+    PR = np.ones(N) / N
+
+
     zero_rows_mask = np.all(transition_matrix == 0, axis=1)
     row_vector = np.where(zero_rows_mask, 1, 0)
     one_vector = np.ones(len(row_vector))
-    Outer_product = np.outer(row_vector, one_vector)
-    Outer_product_matrix =Outer_product / N
+    Outer_product_matrix = np.outer(row_vector, one_vector) / N
+
+
     personalized_vector = np.std(data_matrix, axis=1)
     sum_values = np.sum(personalized_vector)
     if sum_values == 0:
-       normalized_vector = np.zeros_like(personalized_vector) / len(personalized_vector)  
+        normalized_vector = np.zeros_like(personalized_vector) / len(personalized_vector)
     else:
-       normalized_vector = personalized_vector / sum_values
+        normalized_vector = personalized_vector / sum_values
+
     result_matrix = transition_matrix + Outer_product_matrix
-   
+
+
     while True:
-        new_PR = damping_factor * np.dot(result_matrix.T, PR) + factor_2 * normalized_array + factor_1 * normalized_vector
-        if np.linalg.norm(new_PR - PR, 1) < epsilon: 
+        new_PR = (
+            damping_factor * (result_matrix.T @ PR)
+            + factor_2 * normalized_array
+            + factor_1 * normalized_vector
+        )
+
+        if np.linalg.norm(new_PR - PR, 1) < epsilon:
+            PR = new_PR
             break
+
         PR = new_PR
-    PR=PR/np.mean(PR)  
+
+    PR = PR / np.mean(PR)
+
     sorted_indices = np.argsort(PR)[::-1]
+
     index = int(len(PR) * (5 / 100))
+
     PR_mean_value.append(np.mean(PR[sorted_indices[:index]]))
+
     print(k)
     print(PR_mean_value)
 
+labels = ['0', '1', '2', '3', '4', '5']   
 
-labels = ['0', '1', '2', '3','4','5'] 
 plt.plot(labels, PR_mean_value, marker='o', linestyle='-')
 plt.xlabel('Parameter')
-plt.ylabel('High-order PageRank')
+plt.ylabel('HNPP')
 plt.grid(True)
+plt.savefig("HNPP.png", dpi=300, bbox_inches='tight') 
 plt.show()
